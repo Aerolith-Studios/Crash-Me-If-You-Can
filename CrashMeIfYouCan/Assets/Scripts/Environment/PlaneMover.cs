eb228ea5f62992c325b4c827cf75180c3f60b8dd
dunks184 <duncan.g.barr@gmail.com>
2015-08-26 18:20:30 +0930

    Roof Hazards Implemented
    I also made the generation a modular script.

--- CrashMeIfYouCan/Assets/Scripts/Environment/PlaneMover.cs
+++ CrashMeIfYouCan/Assets/Scripts/Environment/PlaneMover.cs
@@ -9,12 +9,6 @@ public class PlaneMover : MonoBehaviour {
 	public float respawnTrigger = -200;
 	public float respawnDist = 1600;
 
-	void Start () {
-//		if (!gameObject.CompareTag ("FirstPlane")) {
-			generateHazards (true);
-//		}
-	}
-
 	// Update is called once per frame
 	void Update () {
 
@@ -23,42 +17,9 @@ public class PlaneMover : MonoBehaviour {
 		transform.position += Vector3.forward * -PlaneSpeed;
 		if (transform.position.z < respawnTrigger) {
 			transform.position += Vector3.forward * respawnDist;
-			generateHazards();
-		}
-	}
-
-	void generateHazards(bool firstSpawn = false) {
-
-		// Get the plane length and divide by the GM/stats hazardDensity to find hazard spacing. Use the first Hazard in the list to get originZ position.
-		float planeLength = transform.localScale.z * 10;
-		float hazardSpacingZ = planeLength / GameObject.Find ("GM").GetComponent<Stats> ().HazardDensity;
-		float spawnPos_HazardZ = transform.position.z - planeLength / 2;
-
-		if (firstSpawn == true && gameObject.CompareTag ("FirstPlane")) {
-			spawnPos_HazardZ = transform.position.z;
-		}
-
-
-		int [] spawnCount = new int[6]; // Keeps track of how many times each hazard is instantiated per plane.
-
-		// Loop instantiates a random hazard, moves forward by the spaceing amount and then repeats until end of plane.
-		while (spawnPos_HazardZ < transform.position.z + planeLength / 2) {
-
-			// Choose a hazard randomly from the list, but make sure each hazard is instantiated no more than twice per plane. (should ammend this later to be function of density).
-			int HazardChoice = Random.Range (0, 6);
-			while (spawnCount[HazardChoice] >= 2) {
-				HazardChoice = Random.Range (0, 6);
-			}
-
-			spawnCount [HazardChoice] ++; // This is outside of the above while loop on purpose.
+			// Destroy Hazards here.
 			
-			GameObject RandomHazard = Hazards [HazardChoice];
-			Vector3 spawnPos_Hazard = new Vector3 (RandomHazard.transform.position.x, RandomHazard.transform.position.y, spawnPos_HazardZ);
-
-			// Instantiate the hazard and child it to the plane under "Hazards".
-			GameObject newHazard = Instantiate (RandomHazard, spawnPos_Hazard, RandomHazard.transform.rotation) as GameObject;
-			newHazard.transform.parent = transform.FindChild ("Hazards");
-			spawnPos_HazardZ += hazardSpacingZ; // loop updater & next HazardZ position.
+			BroadcastMessage("generateHazards", false);
 		}
 	}
 }
